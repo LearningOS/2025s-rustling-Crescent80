653ebffe96e7f7f354a4a94d13753b3b8130d1f0
luoyi <3357977554@qq.com>
2025-03-31 21:17:49 +0800

    struct练习2

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,11 +5,10 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-
 struct ColorClassicStruct {
-    red:i32,
-    green:i32,
-    blue:i32,
+    red: i32,
+    green: i32,
+    blue: i32,
 }
 
 struct ColorTupleStruct(i32, i32, i32);
@@ -23,10 +22,10 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        let green =ColorClassicStruct{
-            red:0,
+        let green = ColorClassicStruct {
+            red: 0,
             green: 255,
-            blue:0,
+            blue: 0,
         };
 
         assert_eq!(green.red, 0);
@@ -36,7 +35,7 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        let green=ColorTupleStruct{0, 255, 0};
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -45,7 +44,7 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        let unit_like_struct =UnitLikeStruct;
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
